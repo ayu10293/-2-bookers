8b74d4c07da1cb71f54342e4146d33eeac5222db
ayu <133560587+ayu10293@users.noreply.github.com@users.noreply.github.com>
2023-07-17 12:48:47 +0000

    [finish]手順28

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
     @books = Book.all
     
     if @book.save
+    flash[:notice] = "Book was successfully created"
     redirect_to book_path(@book.id)
     else
     render :new
@@ -27,9 +28,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @books = Book.all
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy

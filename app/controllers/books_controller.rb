a719f39bbe3944e24c97f0c3618c79c49fdb8965
ayu <133560587+ayu10293@users.noreply.github.com@users.noreply.github.com>
2023-07-17 12:17:09 +0000

    [Finish] 手順27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
   def new
+      @book = Book.new
+      @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :new
+    end
   end
 
   def index
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books/new'
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
